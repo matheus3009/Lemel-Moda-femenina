a1c2a862d72771527290b734f13c425d379797ed
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-06-13 16:02:22 -0300

    lo

---
+++ login.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Login</title>
+    <style>
+        * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        h2{
+            padding-bottom: 5%;
+            
+        }
+
+        h2,
+        h3,
+        h4,
+        main {
+            text-align: center;
+        }
+
+        header {
+            background-color: #bf8869;
+            height: 12vh;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+        }
+
+        main {
+            margin-top: 5%;
+            margin-bottom: 50%;
+        }
+
+        .com {
+            color: black;
+            font-weight: bold;
+            display: inline-block;
+            width: 50%;
+            padding: 1%;
+            border: solid 1px;
+        }
+
+        .caixa {
+            width: 50%;
+        }
+
+        #exemplo-flaxbox {
+            background-color: #26120B;
+            display: flex;
+            flex-wrap: wrap;
+            color: white;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+
+        footer {
+            height: 30px;
+            margin: 0;
+            left: 0;
+        }
+
+        img {
+            width: 20%;
+        }
+
+        .pernome {
+            display: flex;
+            align-items: center;
+            text-align: center;
+
+        }
+
+        .nomeper {
+            margin-left: 10%;
+        }
+
+        .linkper {
+            left: 0%;
+            margin-top: 5%;
+        }
+
+        .links-menu {
+            list-style: none;
+            display: none;
+        }
+
+        .container {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .fg{
+            width: 5%;
+        }
+
+    </style>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1><a href="index.html"><img src="imagens/logo-lemel.png" alt="logo"></a></h1>
+            <nav>
+                <h2><a href="">Menu &equiv;</a></h2>
+                <ul class="links-menu">
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Produtos</a></li>
+                    <li><a href="">Minha conta</a></li>
+                    <li><a href="">Favoritos</a></li>
+                    <li><a href="">Sacolas</a></li>
+                    <li><a href="">Quem somos</a></li>
+                    <li><a href="">Suporte</a></li>
+                </ul>
+            </nav>
+        </div>
+        <form action="resultados.html"><input type="search" id="site-pesquisa" name="q" /><button>Search</button></form>
+        <h2><a href=".html"><img src="" alt="">Login</a></h2>
+        <h2><a href="sacola.html"><img src="" alt="">sacola</a></h2>
+    </header>
+    <main>
+        <h2>Login</h2>
+        <h3>Informe seu E-mail</h3>
+        <br>
+        <div>
+            <form action="perfil.html" method="GET">
+                
+                <label for="nome"></label>
+                <input required placeholder="Digite seu nome" type="nome" name="nome" id="nome">
+                <button type="submit">Confirmar</button>
+            </form>
+            
+        </div>
+<br>
+           <div>
+            <form action="perfil.html" method="GET">
+                
+                <label for="email"></label>
+                <input required placeholder="Digite seu email" type="email" name="email" id="email">
+                <button type="submit">Confirmar</button>
+            </form>
+            
+        </div>
+        <br>
+           <div>
+            <form action="perfil.html" method="GET">
+                
+                <label for="email"></label>
+                <input required placeholder="Digite seu telefone" type="email" name="email" id="email">
+                <button type="submit">Confirmar</button>
+            </form>
+            
+        </div>
+
+        <h4>ou</h4>
+        <h3>Entre com</h3>
+        <p class="com"> <img class="fg" src="/imagens/facebook.png" alt=""> Entra com Facebook</p>
+        <p class="com"> <img class="fg" src="/imagens/google.png" alt=""> Entra com Google</p>
+    </main>
+    <footer>
+        <div id="exemplo-flaxbox">
+            <div class="caixa">
+                <h2>Contato</h2>
+                <ul>
+                    <li>Atendimento</li>
+                    <li>Onde encontrar</li>
+                    <li>Chat Online</li>
+                    <li>SEE ON MAP</li>
+                </ul>
+            </div>
+            <div class="caixa">
+                <h2>Siga</h2>
+                <ul>
+                    <li><a
+                            href="https://www.instagram.com/lemelmodaeacessorios?ig_shid=MTlzemhvcGx2cGM3Zw==">@lemelmodaeacessorios</a>
+                    </li>
+                    <li>SEE ON MAP</li>
+                </ul>
+            </div>
+            <div class="caixa">
+                <img src="imagens/mini-microsoft.jpg" alt="">
+                <h2>FAQ</h2>
+                <ul>
+                    <li>Frete & entrega</li>
+                    <li>Garantia</li>
+                    <li>Forma de pagamento</li>
+                    <li>Troca & devolução</li>
+                    <li>Segurança</li>
+                    <li>Termos & condições</li>
+                </ul>
+            </div>
+            <div class="caixa">
+                <h2>Minha conta</h2>
+                <ul>
+                    <li>Meus dados</li>
+                    <li>Meus pedidos</li>
+                    <li>Minhas trocas</li>
+                    <li>Vale compras</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
