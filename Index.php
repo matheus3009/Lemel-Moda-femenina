f4d6b2d5a2e63cf25114dc4d58b99454629b88c6
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-06-15 11:38:30 -0300

    slider

--- Index.php
+++ Index.php
@@ -118,21 +118,27 @@
         <section>
             <div class="slider-lan">
 
-                <div class="produtos">
+                <div class="produtos-img">
+                
                     <a href="">
-                        <img src="imagens/lemel/334515675_645669600655109_139508241733260701_n.jpg" alt="foto Produtos">
+                        <img class="img-pro" src="imagens/lemel/334515675_645669600655109_139508241733260701_n.jpg" alt="foto Produtos">
                     </a>
-                    <p>testando</p>
+                 
+                    <div class="produtos-text">
+                         <p>produto sla oq</p>
+                         <span>escrita</span>
+                    </div>
                 </div>
-                <div>
+            
+                <div class="produtos-img">
                     <a href="">
-                        <img src="imagens/lemel/334515675_645669600655109_139508241733260701_n.jpg" alt="foto Produtos">
+                        <img class="img-pro" src="imagens/lemel/334515675_645669600655109_139508241733260701_n.jpg" alt="foto Produtos">
                     </a>
                     <p>texto por vir</p>
                 </div>
-                <div>
+                <div class="produtos-img">
                     <a href="">
-                        <img src="imagens/lemel/334515675_645669600655109_139508241733260701_n.jpg" alt="foto Produtos">
+                        <img class="img-pro" src="imagens/lemel/334515675_645669600655109_139508241733260701_n.jpg" alt="foto Produtos">
                     </a>
                     <p>texto por vir</p>
                 </div>
@@ -246,10 +252,10 @@
     <script>
         $(document).ready(function () {
             $('.slider-lan').slick({
-                arrow: true,
                 Infinite: true,
                 slideToShow: 3,
                 slideToScroll: 3
+
             });
         });
     </script>
