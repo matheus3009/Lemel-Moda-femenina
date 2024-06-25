87a844edf968a018316ca665c54ceb40f03b5bdb
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-06-25 16:58:24 -0300

    alterações

---
+++ index-2.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<title>Lojas Lemel</title>
+<link rel="stylesheet" href="slick/slick.css">
+<link rel="stylesheet" href="slick/slick-theme.css">
+
+<body>
+    <main>
+        <h1>Lojas lemel</h1>
+        <!-- slider topo da pagina = inicial -->
+        <section>
+            <div>
+                <div><img src="" alt="">imagens-lemel</div>
+                <div><img src="" alt="">imagens-lemel</div>
+                <div><img src="" alt="">imagens-lemel</div>
+                <div><img src="" alt="">imagens-lemel</div>
+            </div>
+        </section>
+
+        <hr>
+
+        <h2>Categorias</h2>
+        <!-- imagens/links de categoria -->
+        <section>
+            <div>
+                <div>
+                    <a href="">
+                        <img src="" alt="categoria">
+                    </a>
+                    <p>cat-text</p>
+                </div>
+
+                <div>
+                    <a href="">
+                        <img src="" alt="categoria">
+                    </a>
+                    <p>cat-text</p>
+                </div>
+
+                <div>
+                    <a href="">
+                        <img src="" alt="categoria">
+                    </a>
+                    <p>cat-text</p>
+                </div>
+
+                <div>
+                    <a href="">
+                        <img src="" alt="categoria">
+                    </a>
+                    <p>cat-text</p>
+                </div>
+            </div>
+        </section>
+
+        <hr>
+
+        <h2>Lançamentos</h2>
+        <!-- lançamento slider -->
+
+        <section>
+            <!-- conteudo lançamentos -->
+            <div>
+                <div>
+                    <img src="" alt="lançamentos">
+                    <p>slider-lançamentos</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src="" alt="lançamentos">
+                    <p>slider-lançamentos</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src="" alt="lançamentos">
+                    <p>slider-lançamentos</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src="" alt="lançamentos">
+                    <p>slider-lançamentos</p>
+                </div>
+            </div>
+        </section>
+
+        <hr><!-- OBS: lançamentos e ofertas compartilham o mesmo slider -->
+
+        <h2>Ofertas</h2>
+        <!-- slider ofertas -->
+        <section>
+            <!-- conteudo - ofertas -->
+
+            <div>
+                <div>
+                    <img src="" alt="ofertas">
+                    <p>ofertas - slider</p>
+                </div>
+
+                <div>
+                    <img src="" alt="ofertas">
+                    <p>ofertas - slider</p>
+                </div>
+
+                <div>
+                    <img src="" alt="ofertas">
+                    <p>ofertas - slider</p>
+                </div>
+
+                <div>
+                    <img src="" alt="ofertas">
+                    <p>ofertas - slider</p>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="slick/slick.min.js"></script>
+</body>
+
+</html>
